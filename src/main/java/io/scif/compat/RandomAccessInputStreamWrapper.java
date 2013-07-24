9233c433eec85043410c2ec1977ccc474a0fa2d4
Curtis Rueden <ctrueden@wisc.edu>
2013-07-24 11:20:10 -0500

    RandomAccessInputStreamWrapper: remove javadoc
    The javadoc in question did not really add any documentation,
    and was not formatted according to SCIFIO convention, besides.

--- src/main/java/io/scif/compat/RandomAccessInputStreamWrapper.java
+++ src/main/java/io/scif/compat/RandomAccessInputStreamWrapper.java
@@ -42,8 +42,6 @@ class RandomAccessInputStreamWrapper
 
   // -- Constructors --
 
-  /** Wrapper constructor. 
-   * @throws IOException */
   public RandomAccessInputStreamWrapper(io.scif.io.RandomAccessInputStream rais) throws IOException {
     super((String)null);
     this.rais = rais;
