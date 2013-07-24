1508d4446de0e63128aa7c4e158cb438bc2d3a29
Curtis Rueden <ctrueden@wisc.edu>
2013-07-24 11:14:09 -0500

    RandomAccessInputStreamWrapper: add @Overrides
    These are overriding methods from a superclass,
    so they should compile just fine even in Java 5.

--- src/main/java/io/scif/compat/RandomAccessInputStreamWrapper.java
+++ src/main/java/io/scif/compat/RandomAccessInputStreamWrapper.java
@@ -62,16 +62,19 @@ class RandomAccessInputStreamWrapper
    *
    * @see loci.common.Constants#ENCODING
    */
+  @Override
   public void setEncoding(String encoding) {
     unwrap().setEncoding(encoding);
   }
 
   /** Seeks to the given offset within the stream. */
+  @Override
   public void seek(long pos) throws IOException {
     unwrap().seek(pos);
   }
 
   /** Gets the number of bytes in the file. */
+  @Override
   public long length() throws IOException {
     return unwrap().length();
   }
@@ -84,26 +87,31 @@ class RandomAccessInputStreamWrapper
    *
    * Passing in a negative value will reset the length to the stream's real length.
    */
+  @Override
   public void setLength(long newLength) throws IOException {
     unwrap().setLength(newLength);
   }
 
   /** Gets the current (absolute) file pointer. */
+  @Override
   public long getFilePointer() throws IOException {
     return unwrap().getFilePointer();
   }
 
   /** Closes the streams. */
+  @Override
   public void close() throws IOException {
     unwrap().close();
   }
 
   /** Sets the endianness of the stream. */
+  @Override
   public void order(boolean little) {
     unwrap().order(little);
   }
 
   /** Gets the endianness of the stream. */
+  @Override
   public boolean isLittleEndian() {
     return unwrap().isLittleEndian();
   }
@@ -113,6 +121,7 @@ class RandomAccessInputStreamWrapper
    *
    * @see #findString(String...)
    */
+  @Override
   public String readString(String lastChars) throws IOException {
     return unwrap().readString(lastChars);
   }
@@ -126,6 +135,7 @@ class RandomAccessInputStreamWrapper
    *   terminating sequence, or through the end of the stream if no
    *   terminating sequence is found.
    */
+  @Override
   public String findString(String... terminators) throws IOException {
     return unwrap().findString(terminators);
   }
@@ -145,6 +155,7 @@ class RandomAccessInputStreamWrapper
    *   terminating sequence, or through the end of the stream if no
    *   terminating sequence is found, or null if saveString flag is unset.
    */
+  @Override
   public String findString(boolean saveString, String... terminators)
       throws IOException
       {
@@ -162,6 +173,7 @@ class RandomAccessInputStreamWrapper
    *   terminating sequence, or through the end of the stream if no
    *   terminating sequence is found.
    */
+  @Override
   public String findString(int blockSize, String... terminators)
       throws IOException
       {
@@ -184,6 +196,7 @@ class RandomAccessInputStreamWrapper
    *   terminating sequence, or through the end of the stream if no
    *   terminating sequence is found, or null if saveString flag is unset.
    */
+  @Override
   public String findString(boolean saveString, int blockSize,
       String... terminators) throws IOException
       {
@@ -193,81 +206,97 @@ class RandomAccessInputStreamWrapper
   // -- DataInput API methods --
 
   /** Read an input byte and return true if the byte is nonzero. */
+  @Override
   public boolean readBoolean() throws IOException {
     return unwrap().readBoolean();
   }
 
   /** Read one byte and return it. */
+  @Override
   public byte readByte() throws IOException {
     return unwrap().readByte();
   }
 
   /** Read an input char. */
+  @Override
   public char readChar() throws IOException {
     return unwrap().readChar();
   }
 
   /** Read eight bytes and return a double value. */
+  @Override
   public double readDouble() throws IOException {
     return unwrap().readDouble();
   }
 
   /** Read four bytes and return a float value. */
+  @Override
   public float readFloat() throws IOException {
     return unwrap().readFloat();
   }
 
   /** Read four input bytes and return an int value. */
+  @Override
   public int readInt() throws IOException {
     return unwrap().readInt();
   }
 
   /** Read the next line of text from the input stream. */
+  @Override
   public String readLine() throws IOException {
     return unwrap().readLine();
   }
 
   /** Read a string of arbitrary length, terminated by a null char. */
+  @Override
   public String readCString() throws IOException {
     return unwrap().readCString();
   }
 
   /** Read a string of up to length n. */
+  @Override
   public String readString(int n) throws IOException {
     return unwrap().readString(n);
   }
 
   /** Read eight input bytes and return a long value. */
+  @Override
   public long readLong() throws IOException {
     return unwrap().readLong();
   }
 
   /** Read two input bytes and return a short value. */
+  @Override
   public short readShort() throws IOException {
     return unwrap().readShort();
   }
 
   /** Read an input byte and zero extend it appropriately. */
+  @Override
   public int readUnsignedByte() throws IOException {
     return unwrap().readUnsignedByte();
   }
 
   /** Read two bytes and return an int in the range 0 through 65535. */
+  @Override
   public int readUnsignedShort() throws IOException {
     return unwrap().readUnsignedShort();
   }
 
   /** Read a string that has been encoded using a modified UTF-8 format. */
+  @Override
   public String readUTF() throws IOException {
     return unwrap().readUTF();
   }
 
   /** Skip n bytes within the stream. */
+  @Override
   public int skipBytes(int n) throws IOException {
     return unwrap().skipBytes(n);
   }
 
   /** Read bytes from the stream into the given array. */
+  @Override
   public int read(byte[] array) throws IOException {
     return unwrap().read(array);
   }
@@ -275,11 +304,13 @@ class RandomAccessInputStreamWrapper
   /**
    * Read n bytes from the stream into the given array at the specified offset.
    */
+  @Override
   public int read(byte[] array, int offset, int n) throws IOException {
     return unwrap().read(array, offset, n);
   }
 
   /** Read bytes from the stream into the given buffer. */
+  @Override
   public int read(ByteBuffer buf) throws IOException {
     return unwrap().read(buf);
   }
@@ -287,11 +318,13 @@ class RandomAccessInputStreamWrapper
   /**
    * Read n bytes from the stream into the given buffer at the specified offset.
    */
+  @Override
   public int read(ByteBuffer buf, int offset, int n) throws IOException {
     return unwrap().read(buf, offset, n);
   }
 
   /** Read bytes from the stream into the given array. */
+  @Override
   public void readFully(byte[] array) throws IOException {
     unwrap().readFully(array);
   }
@@ -299,28 +332,34 @@ class RandomAccessInputStreamWrapper
   /**
    * Read n bytes from the stream into the given array at the specified offset.
    */
+  @Override
   public void readFully(byte[] array, int offset, int n) throws IOException {
     unwrap().readFully(array, offset, n);
   }
 
   // -- InputStream API methods --
 
+  @Override
   public int read() throws IOException {
     return unwrap().read();
   }
 
+  @Override
   public int available() throws IOException {
     return unwrap().available();
   }
 
+  @Override
   public void mark(int readLimit) {
     unwrap().mark(readLimit);
   }
 
+  @Override
   public boolean markSupported() {
     return unwrap().markSupported();
   }
 
+  @Override
   public void reset() throws IOException {
     unwrap().reset();
   }
