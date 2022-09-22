2bbf3e604f0817bf2d488022b8383e69391c7cc0
Curtis Rueden <ctrueden@wisc.edu>
2022-09-22 17:30:07 -0500

    Happy New Year 2022

--- src/test/java/io/scif/bf/ReadFile.java
+++ src/test/java/io/scif/bf/ReadFile.java
@@ -2,7 +2,7 @@
  * #%L
  * SCIFIO Bio-Formats compatibility format.
  * %%
- * Copyright (C) 2013 - 2020 SCIFIO developers.
+ * Copyright (C) 2013 - 2022 SCIFIO developers.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
